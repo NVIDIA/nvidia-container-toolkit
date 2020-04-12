87c8a868f9f5e0d0e99966fa357085a8c64461ed
Renaud Gaubert <rgaubert@nvidia.com>
2020-04-11 17:18:00 -0700

    Add binary target and use go mod
    Signed-off-by: Renaud Gaubert <rgaubert@nvidia.com>

--- docker.mk
+++ docker.mk
@@ -1,24 +1,5 @@
 # Copyright (c) 2017-2020, NVIDIA CORPORATION. All rights reserved.
 
-DOCKER   ?= docker
-MKDIR    ?= mkdir
-REGISTRY ?= nvidia/container-toolkit
-
-GOLANG_VERSION := 1.14.2
-VERSION        := 1.0.5
-DIST_DIR       := $(CURDIR)/dist
-
-TOOLKIT=nvidia-container-toolkit
-
-include $(CURDIR)/docker.mk
-
-.PHONY: all
-
-all: ubuntu18.04 ubuntu16.04 debian10 debian9 centos7 amzn2 amzn1 opensuse-leap15.1
-
-binary:
-	go build -ldflags "-s -w" -o "$(TOOLKIT)" github.com/NVIDIA/container-toolkit/pkg
-
 push%:
 	docker push "$(REGISTRY)/$*"
 
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/NVIDIA/container-toolkit
+
+go 1.14
+
+require github.com/BurntSushi/toml v0.3.1
