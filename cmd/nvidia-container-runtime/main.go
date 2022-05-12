c77e86137ed148af94a9d41782c75856420af2b1
Evan Lezar <elezar@nvidia.com>
2022-05-12 14:02:06 +0200

    Add version output to CLIs
    This change adds version output to the nvidia-continer-runtime,
    nvidia-container-toolkit, and nvidia-ctk CLIs. The same version
    is used in all cases and includes a version string and a git
    revision if set.
    
    The construction of the version string mirrors what is done in runc.
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- cmd/nvidia-container-runtime/main.go
+++ cmd/nvidia-container-runtime/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/NVIDIA/nvidia-container-toolkit/internal/config"
+	"github.com/NVIDIA/nvidia-container-toolkit/internal/info"
+	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
-var Version string
+// version must be set by go build's -X main.version= option in the Makefile.
+var version = "unknown"
+
+// gitCommit will be the hash that the binary was built from
+// and will be populated by the Makefile
+var gitCommit = ""
 
 var logger = NewLogger()
 
@@ -22,6 +30,11 @@ func main() {
 // run is an entry point that allows for idiomatic handling of errors
 // when calling from the main function.
 func run(argv []string) (rerr error) {
+	printVersion := hasVersionFlag(argv)
+	if printVersion {
+		fmt.Printf("%v version %v\n", "NVIDIA Container Runtime", info.GetVersionString(fmt.Sprintf("spec: %v", specs.Version)))
+	}
+
 	cfg, err := config.GetConfig()
 	if err != nil {
 		return fmt.Errorf("error loading config: %v", err)
@@ -43,5 +56,29 @@ func run(argv []string) (rerr error) {
 		return fmt.Errorf("failed to create NVIDIA Container Runtime: %v", err)
 	}
 
+	if printVersion {
+		fmt.Print("\n")
+	}
 	return runtime.Exec(argv)
+}
+
+// TODO: This should be refactored / combined with parseArgs in logger.
+func hasVersionFlag(args []string) bool {
+	for i := 0; i < len(args); i++ {
+		param := args[i]
+
+		parts := strings.SplitN(param, "=", 2)
+		trimmed := strings.TrimLeft(parts[0], "-")
+		// If this is not a flag we continue
+		if parts[0] == trimmed {
+			continue
+		}
+
+		// Check the version flag
+		if trimmed == "version" {
+			return true
+		}
+	}
+
+	return false
 }
