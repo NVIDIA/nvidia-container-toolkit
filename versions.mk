79510a829007f028174fde2843e44109c2218bad
Evan Lezar <elezar@nvidia.com>
2022-06-02 10:35:48 +0200

    Bump CUDA base image version to 11.7.0
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -24,7 +24,7 @@ NVIDIA_CONTAINER_RUNTIME_VERSION := 3.10.0
 # Specify the expected libnvidia-container0 version for arm64-based ubuntu builds.
 LIBNVIDIA_CONTAINER0_VERSION := 0.10.0+jetpack
 
-CUDA_VERSION := 11.6.0
+CUDA_VERSION := 11.7.0
 GOLANG_VERSION := 1.17.8
 
 GIT_COMMIT ?= $(shell git describe --dirty --long --always 2> /dev/null || echo "")
