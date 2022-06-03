df391e2144807d1734a7c39e545e4a36d08a6bc4
Evan Lezar <elezar@nvidia.com>
2022-06-03 20:34:36 +0200

    Only generate amd64 images for ubuntu18.04
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- build/container/multi-arch.mk
+++ build/container/multi-arch.mk
@@ -30,5 +30,8 @@ push-short:
 # We only have x86_64 packages for centos7
 build-centos7: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
 
+# We only generate amd64 image for ubuntu18.04
+build-ubuntu18.04: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
+
 # We only generate a single image for packaging targets
 build-packaging: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
