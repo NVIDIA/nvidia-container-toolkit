a5bbf613e820c21f6159ae5323c17bdafafde9f2
Evan Lezar <elezar@nvidia.com>
2022-08-10 14:32:44 +0200

    Use single config file for centos, al2, and fedora
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- docker/docker.mk
+++ docker/docker.mk
@@ -105,6 +105,7 @@ LIBNVIDIA_CONTAINER_TAG ?= $(LIB_TAG)
 --centos%: PKG_REV := $(if $(LIB_TAG),0.1.$(LIB_TAG),1)
 --centos%: LIBNVIDIA_CONTAINER_TOOLS_VERSION := $(LIBNVIDIA_CONTAINER_VERSION)-$(if $(LIBNVIDIA_CONTAINER_TAG),0.1.$(LIBNVIDIA_CONTAINER_TAG),1)
 --centos%: DOCKERFILE = $(CURDIR)/docker/Dockerfile.rpm-yum
+--centos%: CONFIG_TOML_SUFFIX := rpm-yum
 --centos8%: BASEIMAGE = quay.io/centos/centos:stream8
 
 # private fedora target
@@ -112,7 +113,7 @@ LIBNVIDIA_CONTAINER_TAG ?= $(LIB_TAG)
 --fedora%: PKG_REV := $(if $(LIB_TAG),0.1.$(LIB_TAG),1)
 --fedora%: LIBNVIDIA_CONTAINER_TOOLS_VERSION := $(LIBNVIDIA_CONTAINER_VERSION)-$(if $(LIBNVIDIA_CONTAINER_TAG),0.1.$(LIBNVIDIA_CONTAINER_TAG),1)
 --fedora%: DOCKERFILE = $(CURDIR)/docker/Dockerfile.rpm-yum
---fedora%: CONFIG_TOML_SUFFIX := centos
+--fedora%: CONFIG_TOML_SUFFIX := rpm-yum
 # The fedora(35) base image has very slow performance when building aarch64 packages.
 # Since our primary concern here is glibc versions, we use the older glibc version available in centos8.
 --fedora35%: BASEIMAGE = quay.io/centos/centos:stream8
@@ -122,6 +123,7 @@ LIBNVIDIA_CONTAINER_TAG ?= $(LIB_TAG)
 --amazonlinux%: LIBNVIDIA_CONTAINER_TOOLS_VERSION := $(LIBNVIDIA_CONTAINER_VERSION)-$(if $(LIBNVIDIA_CONTAINER_TAG),0.1.$(LIBNVIDIA_CONTAINER_TAG),1)
 --amazonlinux%: PKG_REV := $(if $(LIB_TAG),0.1.$(LIB_TAG),1)
 --amazonlinux%: DOCKERFILE = $(CURDIR)/docker/Dockerfile.rpm-yum
+--amazonlinux%: CONFIG_TOML_SUFFIX := rpm-yum
 
 # private opensuse-leap target
 --opensuse-leap%: OS = opensuse-leap
@@ -136,6 +138,7 @@ LIBNVIDIA_CONTAINER_TAG ?= $(LIB_TAG)
 --rhel%: VERSION = $(patsubst rhel%-$(ARCH),%,$(TARGET_PLATFORM))
 --rhel%: ARTIFACTS_DIR = $(DIST_DIR)/rhel$(VERSION)/$(ARCH)
 --rhel%: DOCKERFILE = $(CURDIR)/docker/Dockerfile.rpm-yum
+--rhel%: CONFIG_TOML_SUFFIX := rpm-yum
 --rhel8%: BASEIMAGE = quay.io/centos/centos:stream8
 
 
