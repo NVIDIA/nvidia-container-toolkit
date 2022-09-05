31687185638c8096a7cc4287850bf340a60883e2
Evan Lezar <elezar@nvidia.com>
2022-09-05 15:57:29 +0200

    Update git commit command
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -27,4 +27,4 @@ LIBNVIDIA_CONTAINER0_VERSION := 0.10.0+jetpack
 CUDA_VERSION := 11.7.1
 GOLANG_VERSION := 1.17.8
 
-GIT_COMMIT ?= $(shell git describe --dirty --long --always 2> /dev/null || echo "")
+GIT_COMMIT ?= $(shell git describe --match="" --dirty --long --always 2> /dev/null || echo "")
