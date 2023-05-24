ac11727ec55eee34655c59dddb68db9577d3cddc
Evan Lezar <elezar@nvidia.com>
2023-05-24 10:21:50 +0200

    Add nvidia-contianer-runtime-hook.path config option
    This change adds an nvidia-container-runtime-hook.path config option
    to allow the path used for the prestart hook to be overridden. This
    is useful in cases where multiple NVIDIA Container Toolkit installations
    are present.
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- internal/config/hook.go
+++ internal/config/hook.go
@@ -18,6 +18,9 @@ package config
 
 // RuntimeHookConfig stores the config options for the NVIDIA Container Runtime
 type RuntimeHookConfig struct {
+	// Path specifies the path to the NVIDIA Container Runtime hook binary.
+	// If an executable name is specified, this will be resolved in the path.
+	Path string `toml:"path"`
 	// SkipModeDetection disables the mode check for the runtime hook.
 	SkipModeDetection bool `toml:"skip-mode-detection"`
 }
