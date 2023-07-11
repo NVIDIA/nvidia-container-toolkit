752afe8ca94c948f9e65029eb08e43ea87f2991a
Evan Lezar <elezar@nvidia.com>
2023-07-11 20:32:56 +0200

    Bump version to v1.13.4
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 LIB_NAME := nvidia-container-toolkit
-LIB_VERSION := 1.13.3
+LIB_VERSION := 1.13.4
 LIB_TAG :=
 
 # The package version is the combination of the library version and tag.
