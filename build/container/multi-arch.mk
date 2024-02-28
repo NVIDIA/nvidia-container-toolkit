05dd43848986f3eebc9cda3c178fa3be432410c6
Evan Lezar <elezar@nvidia.com>
2024-02-28 15:45:09 +0200

    Remove centos7 container-toolkit image
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- build/container/multi-arch.mk
+++ build/container/multi-arch.mk
@@ -16,9 +16,6 @@ PUSH_ON_BUILD ?= false
 DOCKER_BUILD_OPTIONS = --output=type=image,push=$(PUSH_ON_BUILD)
 DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64,linux/arm64
 
-# We only have x86_64 packages for centos7
-build-centos7: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
-
 # We only generate amd64 image for ubuntu18.04
 build-ubuntu18.04: DOCKER_BUILD_PLATFORM_OPTIONS = --platform=linux/amd64
 
