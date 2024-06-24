3aeba886d406e8de3c94707e5a3b656b2c71a115
Evan Lezar <elezar@nvidia.com>
2024-06-24 11:12:28 +0200

    Reduce logging for the NVIDIA Container runtime
    This change moves most of the logging for the NVIDIA Container runtime
    from Info to Debug or Trace -- especially in the case where no
    modifications are requried.
    
    This removes execessive logging.
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- internal/runtime/runtime.go
+++ internal/runtime/runtime.go
@@ -17,7 +17,6 @@
 package runtime
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"strings"
@@ -26,6 +25,7 @@ import (
 
 	"github.com/NVIDIA/nvidia-container-toolkit/internal/config"
 	"github.com/NVIDIA/nvidia-container-toolkit/internal/info"
+	"github.com/NVIDIA/nvidia-container-toolkit/internal/logger"
 	"github.com/NVIDIA/nvidia-container-toolkit/internal/lookup/root"
 )
 
@@ -67,23 +67,18 @@ func (r rt) Run(argv []string) (rerr error) {
 		cfg.NVIDIAContainerRuntimeConfig.Mode = r.modeOverride
 	}
 	//nolint:staticcheck  // TODO(elezar): We should swith the nvidia-container-runtime from using nvidia-ctk to using nvidia-cdi-hook.
-	cfg.NVIDIACTKConfig.Path = config.ResolveNVIDIACTKPath(r.logger, cfg.NVIDIACTKConfig.Path)
-	cfg.NVIDIAContainerRuntimeHookConfig.Path = config.ResolveNVIDIAContainerRuntimeHookPath(r.logger, cfg.NVIDIAContainerRuntimeHookConfig.Path)
+	cfg.NVIDIACTKConfig.Path = config.ResolveNVIDIACTKPath(&logger.NullLogger{}, cfg.NVIDIACTKConfig.Path)
+	cfg.NVIDIAContainerRuntimeHookConfig.Path = config.ResolveNVIDIAContainerRuntimeHookPath(&logger.NullLogger{}, cfg.NVIDIAContainerRuntimeHookConfig.Path)
 
-	// Print the config to the output.
-	configJSON, err := json.MarshalIndent(cfg, "", "  ")
-	if err == nil {
-		r.logger.Infof("Running with config:\n%v", string(configJSON))
-	} else {
-		r.logger.Infof("Running with config:\n%+v", cfg)
-	}
+	// Log the config at Trace to allow for debugging if required.
+	r.logger.Tracef("Running with config: %+v", cfg)
 
 	driver := root.New(
 		root.WithLogger(r.logger),
 		root.WithDriverRoot(cfg.NVIDIAContainerCLIConfig.Root),
 	)
 
-	r.logger.Debugf("Command line arguments: %v", argv)
+	r.logger.Tracef("Command line arguments: %v", argv)
 	runtime, err := newNVIDIAContainerRuntime(r.logger, cfg, argv, driver)
 	if err != nil {
 		return fmt.Errorf("failed to create NVIDIA Container Runtime: %v", err)
