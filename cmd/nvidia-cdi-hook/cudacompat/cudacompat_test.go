18c10de9f48f12c90818de144e640cc0e67049c1
Evan Lezar <elezar@nvidia.com>
2025-02-06 17:24:42 +0100

    Add cuda-compat hook to allow compat libs to be discovered
    This change adds an nvidia-cdi-hook cuda-compat hook that checks the
    container for cuda compat libs and updates /etc/ld.so.conf.d to include their
    parent folder if their driver major version is sufficient.
    
    This allows CUDA Forward Compatibility to be used when this is not available
    through the libnvidia-container.
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

---
+++ cmd/nvidia-cdi-hook/cudacompat/cudacompat_test.go
@@ -0,0 +1,114 @@
+/*
+# Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
+package cudacompat
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	testlog "github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompatLibs(t *testing.T) {
+	logger, _ := testlog.NewNullLogger()
+
+	testCases := []struct {
+		description            string
+		contents               map[string]string
+		hostDriverVersion      string
+		expectedCompatContents string
+	}{
+		{
+			description:       "empty root",
+			hostDriverVersion: "222.55.66",
+		},
+		{
+			description: "compat lib is newer; no ldcache",
+			contents: map[string]string{
+				"/usr/local/cuda/compat/libcuda.so.333.88.99": "",
+			},
+			hostDriverVersion: "222.55.66",
+		},
+		{
+			description: "compat lib is newer; ldcache",
+			contents: map[string]string{
+				"/etc/ld.so.cache": "",
+				"/usr/local/cuda/compat/libcuda.so.333.88.99": "",
+			},
+			hostDriverVersion:      "222.55.66",
+			expectedCompatContents: "/usr/local/cuda/compat\n",
+		},
+		{
+			description: "compat lib is older; ldcache",
+			contents: map[string]string{
+				"/etc/ld.so.cache": "",
+				"/usr/local/cuda/compat/libcuda.so.111.88.99": "",
+			},
+			hostDriverVersion:      "222.55.66",
+			expectedCompatContents: "",
+		},
+		{
+			description: "compat lib has same major version; ldcache",
+			contents: map[string]string{
+				"/etc/ld.so.cache": "",
+				"/usr/local/cuda/compat/libcuda.so.222.88.99": "",
+			},
+			hostDriverVersion:      "222.55.66",
+			expectedCompatContents: "",
+		},
+		{
+			description: "driver version empty; ldcache",
+			contents: map[string]string{
+				"/etc/ld.so.cache": "",
+				"/usr/local/cuda/compat/libcuda.so.222.88.99": "",
+			},
+			hostDriverVersion: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			containerRoot := t.TempDir()
+			for name, contents := range tc.contents {
+				target := filepath.Join(containerRoot, name)
+				require.NoError(t, os.MkdirAll(filepath.Dir(target), 0755))
+				require.NoError(t, os.WriteFile(target, []byte(contents), 0600))
+			}
+
+			c := command{
+				logger: logger,
+			}
+			err := c.updateCUDACompatLibs(root(containerRoot), tc.hostDriverVersion)
+			require.NoError(t, err)
+
+			matches, err := filepath.Glob(filepath.Join(containerRoot, "/etc/ld.so.conf.d/00-compat-*.conf"))
+			require.NoError(t, err)
+
+			if tc.expectedCompatContents == "" {
+				require.Empty(t, matches)
+			} else {
+				require.Len(t, matches, 1)
+				contents, err := os.ReadFile(matches[0])
+				require.NoError(t, err)
+
+				require.EqualValues(t, tc.expectedCompatContents, string(contents))
+			}
+		})
+	}
+}
