d80c9733d4f1dfdf9b2f40d2283a193f185499c4
Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>
2025-09-19 16:15:34 +0200

    [no-relnote] rename flush to save
    Signed-off-by: Carlos Eduardo Arango Gutierrez <eduardoa@nvidia.com>

--- pkg/config/engine/containerd/config_drop_in.go
+++ pkg/config/engine/containerd/config_drop_in.go
@@ -71,7 +71,7 @@ func (c *ConfigWithDropIn) Save(dropInPath string) (int64, error) {
 	}
 
 	// TODO: Only do this if we've actually modified the config.
-	if err := c.topLevelConfig.flush(); err != nil {
+	if _, err := c.topLevelConfig.Save(dropInPath); err != nil {
 		return 0, fmt.Errorf("failed to save top-level config: %w", err)
 	}
 
@@ -88,12 +88,13 @@ func (c *ConfigWithDropIn) RemoveRuntime(name string) error {
 
 // flush saves the top-level config to its path.
 // If the config is empty, the file will be deleted.
-func (c *topLevelConfig) flush() error {
-	_, err := c.config.Save(c.path)
-	if err != nil {
-		return fmt.Errorf("failed to flush config to %q: %w", c.path, err)
+func (c *topLevelConfig) Save(dropInPath string) (int64, error) {
+	saveToPath := c.path
+	if dropInPath == "" {
+		saveToPath = ""
 	}
-	return nil
+
+	return c.config.Save(saveToPath)
 }
 
 func (c *topLevelConfig) simplify(dropInFilename string) {
