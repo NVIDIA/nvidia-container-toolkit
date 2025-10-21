767e0b31aa69f7aa8196ac5a572ba73afcb0b2ea
Evan Lezar <elezar@nvidia.com>
2025-10-21 20:22:46 +0200

    Make internal/system packages public
    This change makes the nvdevices and nvmodules packages
    public to allow this functionality to be consumed by other
    clients directly. These include the mig-manager and the
    gpu-operator.
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- cmd/nvidia-ctk-installer/toolkit/toolkit.go
+++ cmd/nvidia-ctk-installer/toolkit/toolkit.go
@@ -30,9 +30,9 @@ import (
 	"github.com/NVIDIA/nvidia-container-toolkit/cmd/nvidia-ctk-installer/container/runtime/crio"
 	"github.com/NVIDIA/nvidia-container-toolkit/cmd/nvidia-ctk-installer/toolkit/installer"
 	"github.com/NVIDIA/nvidia-container-toolkit/internal/logger"
-	"github.com/NVIDIA/nvidia-container-toolkit/internal/system/nvdevices"
 	"github.com/NVIDIA/nvidia-container-toolkit/pkg/nvcdi"
 	transformroot "github.com/NVIDIA/nvidia-container-toolkit/pkg/nvcdi/transform/root"
+	"github.com/NVIDIA/nvidia-container-toolkit/pkg/system/nvdevices"
 )
 
 const (
