7b9c50653960bd36f58dce6a884e242d25023f0d
Evan Lezar <elezar@nvidia.com>
2025-11-19 16:10:14 +0100

    Add --cuda-compat-container-root
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- internal/platform-support/tegra/tegra.go
+++ internal/platform-support/tegra/tegra.go
@@ -80,6 +80,10 @@ func New(opts ...Option) (discover.Discover, error) {
 		return nil, fmt.Errorf("failed to create CSV discoverer: %v", err)
 	}
 
+	// TODO: Set the version here.
+	version := ""
+	cudaCompatLibHookDiscoverer := discover.NewCUDACompatHookDiscoverer(o.logger, o.hookCreator, version)
+
 	ldcacheUpdateHook, err := discover.NewLDCacheUpdateHook(o.logger, csvDiscoverer, o.hookCreator, o.ldconfigPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create ldcach update hook discoverer: %v", err)
@@ -96,6 +100,7 @@ func New(opts ...Option) (discover.Discover, error) {
 
 	d := discover.Merge(
 		csvDiscoverer,
+		cudaCompatLibHookDiscoverer,
 		// The ldcacheUpdateHook is added last to ensure that the created symlinks are included
 		ldcacheUpdateHook,
 		tegraSystemMounts,
