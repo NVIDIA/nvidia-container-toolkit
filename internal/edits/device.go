b4db92f583e94696a92d12aecef4f3631c2ad438
Evan Lezar <elezar@nvidia.com>
2026-02-05 21:35:13 +0100

    fix: Set device node GID in CDI specs
    This change ensures that the GID for a device node is
    set in a generated CDI specification. This is important when
    a device node allows users of the non-root group to access the
    device node -- especially when running in non-root containers.
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- internal/edits/device.go
+++ internal/edits/device.go
@@ -71,6 +71,22 @@ func (d device) fromPathOrDefault() *specs.DeviceNode {
 		}
 	}
 
+	// We construct a CDI spec DeviceNode with the information retrieved.
+	// Note that in addition to the fields that we specify here the following
+	// are not taken from the extracted information:
+	//
+	// * dn.Rule.Allow: This has no equivalent in the CDI spec and is used for
+	//					specifying cgroup rules in a container.
+	// * dn.Rule.Type:  This could be translated to the DeviceNode.Type, but is
+	//					not done. In the toolkit we only consider char devices
+	//					(Type = 'c') and these are the default for device nodes
+	//					in OCI compliant runtimes.
+	// * dn.UID:		This is ignored so as to allow the UID of the container
+	//					user to be applied when making modifications to the OCI
+	//					runtime specification. Note that for most NVIDIA devices
+	//					this would be 0 and as such the target UID pointer will
+	//					remain `nil`.
+	//					See: https://github.com/cncf-tags/container-device-interface/blob/e2632194760242fc74a30c3803107f9c1ba5718b/pkg/cdi/container-edits.go#L96-L100
 	return &specs.DeviceNode{
 		HostPath:    d.HostPath,
 		Path:        d.Path,
@@ -78,5 +94,13 @@ func (d device) fromPathOrDefault() *specs.DeviceNode {
 		Minor:       dn.Minor,
 		FileMode:    &dn.FileMode,
 		Permissions: string(dn.Permissions),
+		GID:         ptrIfNonZero(dn.Gid),
 	}
+}
+
+func ptrIfNonZero(id uint32) *uint32 {
+	if id == 0 {
+		return nil
+	}
+	return &id
 }
