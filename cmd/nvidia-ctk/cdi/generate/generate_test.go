2d761670b7f9d3adec465c4cf41faa86b700ba9d
Tariq Ibrahim <tibrahim@nvidia.com>
2026-09-10 16:53:15 -0700

    feat: add new CDI hook to set CUDA memory limits
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>
    
    get cgroup path from procfs instead
    
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>

--- cmd/nvidia-ctk/cdi/generate/generate_test.go
+++ cmd/nvidia-ctk/cdi/generate/generate_test.go
@@ -98,11 +98,35 @@ devices:
         deviceNodes:
             - path: /dev/nvidia0
               hostPath: {{ .driverRoot }}/dev/nvidia0
+        hooks:
+            - hookName: createRuntime
+              path: /usr/bin/nvidia-cdi-hook
+              args:
+                - nvidia-cdi-hook
+                - apply-cuda-memory-limits
+                - --driver-root
+                - {{ .driverRoot }}
+                - --gpu-id
+                - {{ .gpuID }}
+              env:
+                - NVIDIA_CTK_DEBUG=false
     - name: all
       containerEdits:
         deviceNodes:
             - path: /dev/nvidia0
               hostPath: {{ .driverRoot }}/dev/nvidia0
+        hooks:
+            - hookName: createRuntime
+              path: /usr/bin/nvidia-cdi-hook
+              args:
+                - nvidia-cdi-hook
+                - apply-cuda-memory-limits
+                - --driver-root
+                - {{ .driverRoot }}
+                - --gpu-id
+                - {{ .gpuID }}
+              env:
+                - NVIDIA_CTK_DEBUG=false
 containerEdits:
     env:
         - NVIDIA_CTK_LIBCUDA_DIR=/lib/x86_64-linux-gnu
@@ -200,11 +224,35 @@ devices:
         deviceNodes:
             - path: /dev/nvidia0
               hostPath: {{ .driverRoot }}/dev/nvidia0
+        hooks:
+            - hookName: createRuntime
+              path: /usr/bin/nvidia-cdi-hook
+              args:
+                - nvidia-cdi-hook
+                - apply-cuda-memory-limits
+                - --driver-root
+                - {{ .driverRoot }}
+                - --gpu-id
+                - {{ .gpuID }}
+              env:
+                - NVIDIA_CTK_DEBUG=false
     - name: all
       containerEdits:
         deviceNodes:
             - path: /dev/nvidia0
               hostPath: {{ .driverRoot }}/dev/nvidia0
+        hooks:
+            - hookName: createRuntime
+              path: /usr/bin/nvidia-cdi-hook
+              args:
+                - nvidia-cdi-hook
+                - apply-cuda-memory-limits
+                - --driver-root
+                - {{ .driverRoot }}
+                - --gpu-id
+                - {{ .gpuID }}
+              env:
+                - NVIDIA_CTK_DEBUG=false
 containerEdits:
     env:
         - NVIDIA_CTK_LIBCUDA_DIR=/lib/x86_64-linux-gnu
@@ -294,11 +342,35 @@ devices:
         deviceNodes:
             - path: /dev/nvidia0
               hostPath: {{ .driverRoot }}/dev/nvidia0
+        hooks:
+            - hookName: createRuntime
+              path: /usr/bin/nvidia-cdi-hook
+              args:
+                - nvidia-cdi-hook
+                - apply-cuda-memory-limits
+                - --driver-root
+                - {{ .driverRoot }}
+                - --gpu-id
+                - {{ .gpuID }}
+              env:
+                - NVIDIA_CTK_DEBUG=false
     - name: all
       containerEdits:
         deviceNodes:
             - path: /dev/nvidia0
               hostPath: {{ .driverRoot }}/dev/nvidia0
+        hooks:
+            - hookName: createRuntime
+              path: /usr/bin/nvidia-cdi-hook
+              args:
+                - nvidia-cdi-hook
+                - apply-cuda-memory-limits
+                - --driver-root
+                - {{ .driverRoot }}
+                - --gpu-id
+                - {{ .gpuID }}
+              env:
+                - NVIDIA_CTK_DEBUG=false
 containerEdits:
     env:
         - NVIDIA_CTK_LIBCUDA_DIR=/lib/x86_64-linux-gnu
@@ -539,7 +611,13 @@ containerEdits:
 				require.NoError(t, err)
 			}
 
-			require.Equal(t, strings.ReplaceAll(tc.expectedSpec, "{{ .driverRoot }}", driverRoot), buf.String())
+			gpuID, ret := server.Devices[0].GetUUID()
+			require.True(t, ret == nvml.SUCCESS, gpuID)
+
+			expected := strings.ReplaceAll(tc.expectedSpec, "{{ .driverRoot }}", driverRoot)
+			expected = strings.ReplaceAll(expected, "{{ .gpuID }}", gpuID)
+
+			require.Equal(t, expected, buf.String())
 		})
 	}
 }
